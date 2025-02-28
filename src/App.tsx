c77d72ea9a5d52bc5cb46da690f6b992c206a2fc
anton <avainfoconseils@gmail.com>
2025-02-28 12:13:17 +0100

    refactor(code): Reindent code formatting and add lock files
    - Re-indented and reformatted code for better readability
    - Added `package-lock.json` and `yarn.lock` for dependency consistency

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,21 @@ import logo from './logo.png';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          target="_blank"
-          className="App-link"
-          rel="noopener noreferrer"
-          href="https://www.leadzai.com/"
-        >
-          Weather App
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo"/>
+				<a
+					target="_blank"
+					className="App-link"
+					rel="noopener noreferrer"
+					href="https://www.leadzai.com/"
+				>
+					Weather App
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
