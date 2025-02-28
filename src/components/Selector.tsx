ea5e0129120b75a089639cbe3e00e54d9e32b757
anton <avainfoconseils@gmail.com>
2025-02-28 19:32:06 +0100

    feat(selector): add Selector component with initial styling

---
+++ src/components/Selector.tsx
@@ -0,0 +1,11 @@
+import '../styles/components/Selector.css';
+
+export default function Selector() {
+	return (
+		<select name="city" id="city">
+			<option value="lisbon">Lisbon</option>
+			<option value="paris">Paris</option>
+			<option value="los angeles">Los Angeles</option>
+		</select>
+	);
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,13 @@
 import '../styles/App.css';
 import Header from "../components/Header";
+import Selector from "../components/Selector";
 
 function HomePage() {
 	return (
 		<div className="app">
 			<Header/>
 			<section className="body">
-
+				<Selector/>
 			</section>
 		</div>
 	);
