0f73a0b8bd380e4081828404e8762a9c9b2b108f
anton <avainfoconseils@gmail.com>
2025-02-28 21:55:17 +0100

    style(temperature): fix margin of temperature text

--- src/components/Temperature.tsx
+++ src/components/Temperature.tsx
@@ -1,6 +1,6 @@
 export function Temperature(props: { isFahrenheit: boolean }) {
 	return (
-		<h2 style={{fontSize: "10vw", color: "white"}}>
+		<h2 style={{fontSize: "10vw", color: "white", margin: "5vh 0"}}>
 			15{props.isFahrenheit ? "°F" : "°C"}
 		</h2>
 	);
