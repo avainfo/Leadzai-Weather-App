cf31c28b115eab4617b707b9c4449edd5742a2be
anton <avainfoconseils@gmail.com>
2025-03-05 15:38:28 +0100

    fix(UnitSelector): fix maths

--- src/components/UnitSelector.tsx
+++ src/components/UnitSelector.tsx
@@ -20,7 +20,7 @@ export default function UnitSelector({onToggle}: { onToggle: (b: boolean) => voi
 			<p>°C</p>
 			<div className="switch">
 				<span className="slider" style={{
-					left: "calc(30px / 4 * " + (isChecked ? "3" : "-1") + ")"
+					left: "calc(30px / 4 * " + (isChecked ? "4" : "-1") + ")"
 				}} onClick={handleToggle}></span>
 			</div>
 			<p>°F</p>
